ba51d9f5580b60a7617191da6cd3de1b68b5f7b3
munash48 <mugwanyan@gmail.com>
2019-10-21 19:42:08 +0300

    initialization of spring boot

---
+++ billionareskb/src/main/java/com/kalimagezi/billionareskb/BillionareskbApplication.java
@@ -0,0 +1,13 @@
+package com.kalimagezi.billionareskb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillionareskbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillionareskbApplication.class, args);
+	}
+
+}
