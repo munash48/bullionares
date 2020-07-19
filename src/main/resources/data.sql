53bfbdf591914b2a3f9424aa678c851ebda11e31
munash48 <mugwanyan@gmail.com>
2020-07-19 19:51:46 +0300

    make it better kalmanare default data last

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,14 @@
-INSERT INTO `category`( `cat_name`, `description`, `enabled`) VALUES ('ADMIN','All admin related activiies',false);
-INSERT INTO `category`( `cat_name`, `description`, `enabled`) VALUES ('SPORTS','All sports relating activities',true);
-INSERT INTO `category`( `cat_name`, `description`, `enabled`) VALUES ('INOVATION','All technology related innovations',true);
-INSERT INTO `category`( `cat_name`, `description`, `enabled`) VALUES ('AGRICULTURE','All agriculture related activities',true);
-INSERT INTO `category`( `cat_name`, `description`, `enabled`) VALUES ('WRITING','All writing related activiies',true);
-INSERT INTO `category`( `cat_name`, `description`, `enabled`) VALUES ('ACTING','All acting related activiies',true);
-INSERT INTO `category`( `cat_name`, `description`, `enabled`) VALUES ('EDUCATION','All Education related activiies',true);
-INSERT INTO `category`( `cat_name`, `description`, `enabled`) VALUES ('DBATE','All debate related activiies',true);
-INSERT INTO `category`( `cat_name`, `description`, `enabled`) VALUES ('POLITICS','All leardership related activiies',true);
-INSERT INTO `category`( `cat_name`, `description`, `enabled`) VALUES ('RELIGION','All Religious related activiies',true);
-INSERT INTO `category`( `cat_name`, `description`, `enabled`) VALUES ('ART','All Atistic related activiies',true);
-INSERT INTO `category`( `cat_name`, `description`, `enabled`) VALUES ('MUSIC','All Music related activiies',true);
-
-INSERT INTO `user_details`(`catid`, `email`, `enabled`, `first_name`, `image_link`, `other_names`, `password`, `role`) VALUES (1,'mugwanyan@kyu.ac.ug',true,'MUGWANYA','','NASSER','$2y$12$OCF8e/w5wVCtIEqpv9dz.OVuFyBTHF/IMUK9B/iPhlDWqOeTy.D/W','ROLE_ADMIN');
+INSERT INTO category( cat_name, description, enabled) VALUES ('ADMIN','All admin related activiies',false);
+INSERT INTO category( cat_name, description, enabled) VALUES ('SPORTS','All sports relating activities',true);
+INSERT INTO category( cat_name, description, enabled) VALUES ('INOVATION','All technology related innovations',true);
+INSERT INTO category( cat_name, description, enabled) VALUES ('AGRICULTURE','All agriculture related activities',true);
+INSERT INTO category( cat_name, description, enabled) VALUES ('WRITING','All writing related activiies',true);
+INSERT INTO category( cat_name, description, enabled) VALUES ('ACTING','All acting related activiies',true);
+INSERT INTO category( cat_name, description, enabled) VALUES ('EDUCATION','All Education related activiies',true);
+INSERT INTO category( cat_name, description, enabled) VALUES ('DBATE','All debate related activiies',true);
+INSERT INTO category( cat_name, description, enabled) VALUES ('POLITICS','All leardership related activiies',true);
+INSERT INTO category( cat_name, description, enabled) VALUES ('RELIGION','All Religious related activiies',true);
+INSERT INTO category( cat_name, description, enabled) VALUES ('ART','All Atistic related activiies',true);
+INSERT INTO category( cat_name, description, enabled) VALUES ('MUSIC','All Music related activiies',true);
 
 
