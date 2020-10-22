cae129804ccfa513dc468aa09da2e5377c848a0b
mugwa <mugwa@10.40.0.156>
2020-10-22 18:07:00 +0300

    10 22 2020 commited from office

--- src/main/resources/static/bower_components/jquery/js/jquery.save2.js
+++ src/main/resources/static/bower_components/jquery/js/jquery.save2.js
@@ -1,8 +1,8 @@
 $(function() {
-
+	alert("save 2 is active");
 	$("#profileEditFrm").submit(function(e) {
-		alert("happeing by jquery");
 		e.preventDefault();
+		alert("happeing by jquery");	
 		var frm = $("#profileEditFrm");
 		var data = {};
 		$.each(this, function(i, v) {
@@ -10,49 +10,74 @@ $(function() {
 			data[input.attr("name")] = input.val();
 			delete data["undefined"];
 
-		});		
-		saveRequestedData(frm, data);
+		});	
+		
+		saveProfileData(frm, data);
 	});
 	
-	$("#companyEditFrm").submit(function(e) {
-		
-		alert("happeing by jquery");
-		
-		e.preventDefault();
-		var frm = $("#companyEditFrm");
-		var data = {};
-		$.each(this, function(i, v) {
-			var input = $(v);
-			data[input.attr("name")] = input.val();
-			delete data["undefined"];
-			
-		});		
-		saveRequestedData(frm, data);
-				
-	});
+//	$("#companyEditFrm").submit(function(e) {
+//		
+//		alert("happeing by jquery");
+//		
+//		e.preventDefault();
+//		var frm = $("#companyEditFrm");
+//		var data = {};
+//		$.each(this, function(i, v) {
+//			var input = $(v);
+//			data[input.attr("name")] = input.val();
+//			delete data["undefined"];
+//			
+//		});		
+//		saveRequestedData(frm, data,);
+//				
+//	});
 	
 });
 
-function saveRequestedData(frm, data,nurl){
-	
+//function saveProfileData(frm, data){
+//	
+//	 $.ajax({
+//		 contentType (default: 'application/x-www-form-urlencoded; charset=UTF-8'),
+////		 contentType: "application/json",
+//		 url:frm.attr("action"),
+//	     type:frm.attr("method"),
+//	     data: {
+//	        "id":id,
+//	        "firstName":firstName,
+//	        "otherNames":otherNames,
+//	        "phoneNumber":phoneNumber,
+//	        "email":email,
+//	        "profession":profession,
+//	        "birthDate":birthDate
+//	     },
+//	     success: function(response){
+//			
+//	        alert(data.message);
+//	        
+//	        getHomePage("/profile");
+//
+//	     },
+//	     error:function(response){
+//	    	 
+//	         alert("something went wrong");
+//	     }
+//	 });
+//}
+
+function saveUpdatedData(frm, data){		
 	 $.ajax({
 		 contentType (default: 'application/x-www-form-urlencoded; charset=UTF-8'),
 //		 contentType: "application/json",
 		 url:frm.attr("action"),
 	     type:frm.attr("method"),
 	     data: {
-	        "id": id,
-	        "firstName":firstName,
-	        "otherNames":otherNames,
-	        "phoneNumber":phoneNumber,
-	        "email":email,
-	        "profession":profession,
-	        "birthDate":birthDate
+	        "resetcode": resetcode,
+	        "password":password,
+	        "confirm":confirm
 	     },
 	     success: function(response){
 			
-	        alert(data.message);
-	        
+	        alert("sucess");
 	        getPage(nurl);
 
 	     },
@@ -61,6 +86,5 @@ function saveRequestedData(frm, data,nurl){
 	     }
 	 });
 }
-
 		
 
