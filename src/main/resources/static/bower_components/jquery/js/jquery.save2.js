4b74111aeb5012174ef4ff57ee7a23db887d5e9b
munash48 <mugwanyan@gmail.com>
2020-10-22 20:35:29 +0300

    10 22 2020 from home content type failed.

--- src/main/resources/static/bower_components/jquery/js/jquery.save2.js
+++ src/main/resources/static/bower_components/jquery/js/jquery.save2.js
@@ -1,90 +1,54 @@
 $(function() {
-	alert("save 2 is active");
+	alert("inside save 2");
+	
 	$("#profileEditFrm").submit(function(e) {
 		e.preventDefault();
-		alert("happeing by jquery");	
+		alert("submit by jquery");
 		var frm = $("#profileEditFrm");
 		var data = {};
 		$.each(this, function(i, v) {
 			var input = $(v);
 			data[input.attr("name")] = input.val();
 			delete data["undefined"];
-
-		});	
+			
+		});
 		
-		saveProfileData(frm, data);
+		var nurl="/profile";
+		
+		saveUpdatedData(frm, data,nurl);
+		
+		
 	});
 	
-//	$("#companyEditFrm").submit(function(e) {
-//		
-//		alert("happeing by jquery");
-//		
-//		e.preventDefault();
-//		var frm = $("#companyEditFrm");
-//		var data = {};
-//		$.each(this, function(i, v) {
-//			var input = $(v);
-//			data[input.attr("name")] = input.val();
-//			delete data["undefined"];
-//			
-//		});		
-//		saveRequestedData(frm, data,);
-//				
-//	});
-	
+
 });
 
-//function saveProfileData(frm, data){
-//	
-//	 $.ajax({
-//		 contentType (default: 'application/x-www-form-urlencoded; charset=UTF-8'),
-////		 contentType: "application/json",
-//		 url:frm.attr("action"),
-//	     type:frm.attr("method"),
-//	     data: {
-//	        "id":id,
-//	        "firstName":firstName,
-//	        "otherNames":otherNames,
-//	        "phoneNumber":phoneNumber,
-//	        "email":email,
-//	        "profession":profession,
-//	        "birthDate":birthDate
-//	     },
-//	     success: function(response){
-//			
-//	        alert(data.message);
-//	        
-//	        getHomePage("/profile");
-//
-//	     },
-//	     error:function(response){
-//	    	 
-//	         alert("something went wrong");
-//	     }
-//	 });
-//}
+function saveUpdatedData(frm, data,nurl){		
+		 $.ajax({
+			 contentType (default: 'application/x-www-form-urlencoded; charset=UTF-8'),
+	//	 contentType: "application/json",
+			 url:frm.attr("action"),
+		     type:frm.attr("method"),
+		     data: {
+		        "id": id,
+		        "firstName":firstName,
+		        "otherNames":otherNames,
+		        "phoneNumber":phoneNumber,
+		        "email":email,
+		        "profession":profession,
+		        "birthDate":birthDate
+		     },
+		     success: function(response){
+				
+		        alert("sucess");
+		        getHomePage(nurl);
 
-function saveUpdatedData(frm, data){		
-	 $.ajax({
-		 contentType (default: 'application/x-www-form-urlencoded; charset=UTF-8'),
-//		 contentType: "application/json",
-		 url:frm.attr("action"),
-	     type:frm.attr("method"),
-	     data: {
-	        "resetcode": resetcode,
-	        "password":password,
-	        "confirm":confirm
-	     },
-	     success: function(response){
-			
-	        alert("sucess");
-	        getPage(nurl);
-
-	     },
-	     error:function(response){
-	         alert("something went wrong");
-	     }
-	 });
+		     },
+		     error:function(response){
+		         alert("something went wrong");
+		     }
+		 });
 }
-		
+
+
 
